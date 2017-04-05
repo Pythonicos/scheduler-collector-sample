47069ebdef680f40d6a5039325bacf21e8f4dbd3
orlando <ohrlando@gmail.com>
2017-04-05 20:59:54 -0300

    rename schedule folder create setup.py

---
+++ schedule_collector/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="scheduler-collector",
+      description="A scheduler for helping and monitoring collect and senders",
+      url="http://github.com/ohrlando/schedule_collector",
+      author="Orlando",
+      packages=["schedule-collector"])
